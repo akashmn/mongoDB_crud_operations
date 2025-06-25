12a7987f52ebdc84fa714ba332c9d19040ca100c
Akash M Nandan <akash003nandan@gmail.com>
2025-06-25 23:52:14 +0530

    design for the form set up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const userModel = require('./models/user'); //model/user.js
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -14,6 +15,10 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
