e67a932d2d31431e13c2377be15c37a2b2f115da
Akash M Nandan <akash003nandan@gmail.com>
2025-06-25 23:35:35 +0530

    boiler plate set up

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+////////////////////////routes////////////////////////
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
